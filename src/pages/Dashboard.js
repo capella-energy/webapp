eaa6b73fd3ddc8489b225cd724b9e65a43168b48
capella-energy <allofthebirdiesinrio2@gmail.com>
2021-03-22 00:43:57 -0400

    added slider

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,35 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
+import Slider from "react-animated-slider";
+import "react-animated-slider/build/horizontal.css";
+import "../assets/Dashboard.css";
+import "../assets/slider-animations.css";
+
+import { SliderData } from "./LearnGalleryData";
 
 export default function Dashboard() {
   return (
     <div>
       <Navbar />
       <h1 className="section-titles">Welcome, Hans</h1>
+
+      <Slider className="slider-wrapper">
+        {SliderData.map((item, index) => (
+          <div
+            key={index}
+            className="slider-content"
+            style={{
+              background: `url('${item.image}') no-repeat center center`,
+            }}
+          >
+            <div className="inner">
+              <h1>{item.title}</h1>
+              <p>{item.description}</p>
+              <button>{item.button}</button>
+            </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 }
--- src/pages/LearnGalleryData.jsx
+++ src/pages/LearnGalleryData.jsx
@@ -2,6 +2,26 @@ import image1 from "../assets/solarimage1.webp"
 import image2 from "../assets/solarimage2.jpeg"
 import image3 from "../assets/solarimage3.jpeg"
 import image4 from "../assets/solarimage4.jpg"
+import image5 from "../assets/slide3.png";
+import image6 from "../assets/slide2.png";
+
+const image11 = "../assets/slide2.png";
+
+export const SliderData = [
+    {
+        title: "Create Your Green Energy Partnership Today",
+        description: "Hello",
+        image: 'https://images.squarespace-cdn.com/content/v1/5223f925e4b02f25c1dc75df/1576122208794-Q3AN7DZHXNH50RNAXI4F/ke17ZwdGBToddI8pDm48kA8OHtqHjQ9gYIreuFB3lrwUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYy7Mythp_T-mtop-vrsUOmeInPi9iDjx9w8K4ZfjXt2dhgb8oQmvBlPjkLJ-8A7pJNmoUVr7-vGB5fHg8rmz_2noRwB-dUGsSquCnVTFQcaRg/Solar-Panels-city-scape.jpg?format=2500w',
+        button: "Go to Marketplace"
+    },
+    {
+        title: "Getting to Know Community Solar",
+        description: "Hello",
+        image: "https://www.sgrlaw.com/wp-content/uploads/2018/04/Homepage_News-Blog-Solar-Energy-in-City.jpg",
+        button: "Learn"
+    
+    }
+]
 
 export const LearnGalleryData = [
     {
