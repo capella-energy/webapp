337d537333667d7d94663eb70fb68d5b95526312
bemin.shaker <beminshaker1@outlook.com>
2021-04-08 16:19:38 -0400

    Change pic link

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -82,7 +82,7 @@ export default function Map() {
               lng: project.geometry.coordinates[0]
             }}
             icon={{
-              url: "https://lh3.googleusercontent.com/LfZrWPHfY3E7bZ675N-yu7m2AvgI39fxPePN_PZ4OKZtouJTuHKmA8JHbNGLSMfeMgkYnuFcusiyAd7leZzqKrTX4Li6_dzvx57hUPfhaEsLsMGJE1dCtb1TWIFpaYtsjjQn4kc1N1Pp6FT4n808Te91lUUMnQ3f_7z9wu_pQKaTcg0mYQSgxpf7rn5ewNIQqpptxZXpYQSHtyh4FcmE_7yjkCitPNa8AcZLZgFP14vcecJTrKfEJcnf6ZllZLp6dfyZ9GIKtEOS0NBOn25pHTfXiwYJGPPI9OZ77b0wOJXZJ9keHcf6OZSW9zk_VMUNiw2QEBIf5Qo21uDKnwma2BepetISYCQ471hhtdtTTYnWbMM6QlJ-wFQ9J_oeMvDfcDnrjxC7HNVfqg9fXVMoP2tCJ4bI73Y-iMA2grOPnfuD555Z_UVNkOyzO4zRnllWZrWp-iT1gPu2FcPBnKO-LcZEC0TJ3BIYPtde1BNayG5aX0IlFxYiLQ2Ltw6neUy5-zSk5Y9xwX-TGSo3rPmyxVbp-PIsjyI0XbCj0qIPlr_vOwXGkrnN4ga1_Os6YxEFTDJwx-rfVQDwkKlS987d2HFNoR8V1HTzYvPo3SYOaF3rSak_h_79v8Nqi4A2nVZzYm9TMUslE8OlYToFLEDorWziFP2X0bzIvjY-ASA_dQJgC9syalIBwqcEN5Md8bjs0ge5FDI7D1FkJDLOJKCvgQ=s32-no?authuser=2", 
+              url: "https://lh3.googleusercontent.com/pw/ACtC-3e8EOgzBylIA5SjWcbSKz97jK-it2NHTMNAa2E_21YMvh2vDpzMsD0C__ppUbU3RpUTUY1GJb3XWwc5VB3fU1Mc-ZMc6WR3qHunrA_zzSEijVeJvDZisPIGTvruRhrX0nKk94C8HfuNBtBV0hSgikQ=s32-no?authuser=2", 
               scaledSize: new window.google.maps.Size(35, 35),
               origin: new window.google.maps.Point(0, 0), 
               anchor: new window.google.maps.Point(15, 15) 
