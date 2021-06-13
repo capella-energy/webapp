e788150999e338f540ca0e5b0952368616fc73c5
bemin.shaker <beminshaker1@outlook.com>
2021-06-13 03:14:33 -0400

    project details

--- src/pages/Marketplace/ProjectDetails.jsx
+++ src/pages/Marketplace/ProjectDetails.jsx
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
 import firebase from 'firebase'; 
+import "./ProjectDetails.css";
+import Navbar from "../Navbar/Navbar";
 
+
+const db = firebase.firestore();
 
 
 class ProjectDetails extends Component {
-    handleSubmit = () => {
 
-    }; 
-
-
-    render() { 
-
-        const selectedProj = this.props.match.params.id;
-        const db = firebase.firestore().collection('Projects').doc(selectedProj);
+    state = {
+        projects: [],
+        projectName: "",
+        projectLocation: ""
+      }
     
 
-       
+    handleSubmit = () => {
+
+    };
+
+    render() { 
+        const selectedProj = this.props.match.params.id
+        var docRef = db.collection("Projects").doc(selectedProj);
+
+        docRef.get().then((doc) => {
+             this.setState({
+                projectName: doc.data().Name,
+                projectLocation: doc.data().Location
+              })
+        })
+   
         return (
-            <div> 
-                <h1>Hello</h1>
+            <div className="projDetailsPage"> 
+                <h3>{this.state.projectName}</h3>
+                <h3>{this.state.projectLocation}</h3>
             </div>
+            
           );
         
     }
